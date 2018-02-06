2d8c475f59c9c07567833c11e4b6e87681c06c26
mlk777 <>
2018-02-07 00:23:07 +0100

    deploying project on github pages

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 20) {
+    $('#navbar').addClass('disappear');
+    $('#backToTop').fadeIn(200);
+  }
+  else {
+    $('#navbar').removeClass('disappear');
+    $('#backToTop').fadeOut(200);
+  }
+});
+
+$('#backToTop').click(function () {
+  $('html').animate({
+    scrollTop: 0
+  }, 500);
+});
+
+$(document).ready(function () {
+  $("a").on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+        window.location.hash = hash;
+      });
+    }
+  });
+});
